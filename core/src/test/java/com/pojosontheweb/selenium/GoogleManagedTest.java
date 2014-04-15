9b798b1fc4e25e791763bcf8c6a229cc4393e9a5
Remi Vankeisbelck <remi@rvkb.com>
2014-04-15 14:50:09 +0200

    screenshot

--- core/src/test/java/com/pojosontheweb/selenium/GoogleManagedTest.java
+++ core/src/test/java/com/pojosontheweb/selenium/GoogleManagedTest.java
@@ -1,12 +1,23 @@
 package com.pojosontheweb.selenium;
 
+import org.apache.commons.io.FileUtils;
 import org.junit.Test;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import java.io.File;
 
 public class GoogleManagedTest extends ManagedDriverTestBase {
 
     @Test
-    public void testWithInjectedWebDriver() {
-        GoogleRawTest.performTest(getWebDriver());
+    public void testWithInjectedWebDriver() throws Exception {
+        try {
+            GoogleRawTest.performTest(getWebDriver());
+        } catch(Exception e) {
+            File scrFile = ((TakesScreenshot)getWebDriver()).getScreenshotAs(OutputType.FILE);
+            FileUtils.copyFile(scrFile, new File("/tmp/testWithInjectedWebDriver.png"));
+            throw e;
+        }
     }
 
 
