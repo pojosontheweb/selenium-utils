6d913afef9143bee9a8a5dfd1d2c3fc1b474436b
Remi Vankeisbelck <remi@rvkb.com>
2015-03-26 02:28:55 +0100

    docker : working version FF and video

--- taste/docker/run.sh
+++ taste/docker/run.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+# kill `pidof Xvfb`
 /usr/bin/Xvfb :99 -screen 0 1024x768x24 +extension RANDR &
-taste -d /mnt/target/google -c /mnt/cfg.taste /mnt/google.taste
+taste -d /mnt/target -c /mnt/cfg.taste /mnt/tests.taste
