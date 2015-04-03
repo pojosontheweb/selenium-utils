5a000c99c7e742bedc56b41a1d08ddb7cf0fef05
Remi Vankeisbelck <remi@rvkb.com>
2015-04-03 15:32:45 +0200

    Dockerfile for taste-cloud

--- taste-cloud/docker/build.sh
+++ taste-cloud/docker/build.sh
@@ -3,5 +3,5 @@ TARGET=../target
 DOCKER_TARGET=$TARGET/docker
 mkdir $DOCKER_TARGET
 cp Dockerfile $DOCKER_TARGET
-cp $TARGET/taste-cloud.war $DOCKER_TARGET
+cp taste-cloud.war $DOCKER_TARGET
 sudo docker build -t taste-cloud $DOCKER_TARGET
