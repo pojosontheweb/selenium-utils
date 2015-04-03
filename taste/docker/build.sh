a8f4e0a2a10f18fda889d640a30e046d7f3d9364
Remi Vankeisbelck <remi@rvkb.com>
2015-04-03 14:43:19 +0200

    refactor docker stuff

--- taste/docker/build.sh
+++ taste/docker/build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-mkdir ../target/docker
-cp Dockerfile ../target/docker
-cp run-taste.sh ../target/docker
-cp ../target/taste-1.0-beta4-bin.tar.gz ../target/docker
-sudo docker build -t taste ../target/docker
+TARGET=../../target/docker
+mkdir $TARGET
+cp Dockerfile $TARGET
+cp run-taste.sh $TARGET
+cp $TARGET/taste-1.0-beta4-bin.tar.gz .$TARGET/docker
+sudo docker build -t taste $TARGET/docker
