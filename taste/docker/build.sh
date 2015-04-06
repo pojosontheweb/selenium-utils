801a42e1deea2dbca0cc991b2d31bb5ef27a3488
Remi Vankeisbelck <remi@rvkb.com>
2015-04-06 18:14:23 +0200

    docker stuff

--- taste/docker/build.sh
+++ taste/docker/build.sh
@@ -5,4 +5,4 @@ mkdir $DOCKER_TARGET
 cp Dockerfile $DOCKER_TARGET
 cp run-taste.sh $DOCKER_TARGET
 cp $TARGET/taste-1.0-beta4-bin.tar.gz $DOCKER_TARGET
-sudo docker build -t taste $DOCKER_TARGET
+docker build -t taste $DOCKER_TARGET
