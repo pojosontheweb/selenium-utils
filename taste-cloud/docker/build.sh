ee9661d55d0db654c3417a30349095ea15685e5f
Remi Vankeisbelck <remi@rvkb.com>
2015-04-14 18:33:36 +0200

    docker plugin

--- taste-cloud/docker/build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-TARGET=../target
-DOCKER_TARGET=$TARGET/docker
-mkdir $DOCKER_TARGET
-cp Dockerfile $DOCKER_TARGET
-cp $TARGET/taste-cloud.war $DOCKER_TARGET
-docker build -t taste-cloud $DOCKER_TARGET
