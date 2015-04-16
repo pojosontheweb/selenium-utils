4521db1b77ff3db7dc432c77d33e58eb60d4e8e1
Remi Vankeisbelck <remi@rvkb.com>
2015-04-16 10:40:39 +0200

    deps & cleanup

--- taste-cloud/src/main/groovy/com/pojosontheweb/tastecloud/model/Taste.groovy
+++ taste-cloud/src/main/groovy/com/pojosontheweb/tastecloud/model/Taste.groovy
@@ -1,7 +1,5 @@
 package com.pojosontheweb.tastecloud.model
 
-import com.pojosontheweb.selenium.Browsr
-
 import javax.persistence.Column
 import javax.persistence.Entity
 import javax.persistence.Id
