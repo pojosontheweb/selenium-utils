47f8cbb9336252642249bd75fe8302479ffbedf3
Remi Vankeisbelck <remi@rvkb.com>
2015-05-28 17:55:47 +0200

    args for run-node

--- gridvideo/docker/run-node.sh
+++ gridvideo/docker/run-node.sh
@@ -1,9 +1,24 @@
 #!/usr/bin/env bash
+
 if [ -z "$1" ]
   then
-	  NODE_PORT=5555
+	NODE_PORT=5555
   else
-	  NODE_PORT=$1
+	NODE_PORT=$1
+fi
+
+if [ -z "$2" ]
+  then
+    HOST_IP=`/sbin/ip route|awk '/default/ { print $3 }'`
+  else
+    HOST_IP=$2
+fi
+
+if [ -z "$3" ]
+  then
+    HUB_URL="http://${HOST_IP}:4444/grid/register"
+  else
+    HUB_URL=$3
 fi
 
 echo "Starting XVFB"
@@ -16,11 +31,8 @@ done
 dbus-uuidgen > /var/lib/dbus/machine-id
 export CHROME_DEVEL_SANDBOX=
 
-HOST_IP=`/sbin/ip route|awk '/default/ { print $3 }'`
-
-HUB_URL="http://${HOST_IP}:4444/grid/register"
 echo "XVFB started, starting node and registering to ${HUB_URL}"
 
 cd /grid
-java -Djava.security.egd=file:///dev/urandom -cp *:. -Dwebdriver.chrome.driver=/grid/chromedriver org.openqa.grid.selenium.GridLauncher -role node -hub ${HUB_URL} -maxSession 1 -port ${NODE_PORT} -debug
+java -Djava.security.egd=file:///dev/urandom -cp *:. -Dwebdriver.chrome.driver=/grid/chromedriver org.openqa.grid.selenium.GridLauncher -role node -hub ${HUB_URL} -maxSession 1 -port ${NODE_PORT} -debug -host ${HOST_IP}
 
