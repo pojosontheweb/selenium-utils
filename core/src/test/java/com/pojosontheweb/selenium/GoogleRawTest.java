71e370d7044d32bc42f0776c88e9163687c2cdf4
Rémi Van Keisbelck <remi@rvkb.com>
2024-04-05 09:53:30 +0200

    fix google test

--- core/src/test/java/com/pojosontheweb/selenium/GoogleRawTest.java
+++ core/src/test/java/com/pojosontheweb/selenium/GoogleRawTest.java
@@ -58,7 +58,7 @@ public class GoogleRawTest {
 
         g
                 .dismissCookies()
-                .typeQuery("pojosontheweb")
+                .typeQuery("selenium-utils github pojosontheweb")
                 .assertHasResult("pojosontheweb/selenium-utils");
 
         // assert we have clicked
