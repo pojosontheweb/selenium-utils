60e6123d107ba35aeacd67a6978779c937cdb7b4
Frank Wagner <frank.wagner@fr.ibm.com>
2025-07-06 14:12:20 +0200

    cleanup

--- core/src/main/java/com/pojosontheweb/selenium/ScreenRecordr.java
+++ core/src/main/java/com/pojosontheweb/selenium/ScreenRecordr.java
@@ -64,44 +64,4 @@ public class ScreenRecordr implements VideoRecordr {
         return Arrays.asList(files);
     }
 
-    public ScreenRecordr moveVideoFilesTo(File destDir, String filePrefix) {
-        stop();
-        List<File> files = getVideoFiles();
-        Findr.logDebug("[ScreenRecordr] moving " + files.size() + " video files to " + destDir +
-                " with filePrefix=" + filePrefix);
-        int totalCount = 1;
-        boolean needsCount = files.size() > 1;
-        for (File f : files) {
-            if (f.exists()) {
-                String fileName = filePrefix;
-                if (needsCount) {
-                    fileName += "-" + totalCount++;
-                }
-                fileName += ".mov";
-                if (!destDir.exists()) {
-                    destDir.mkdirs();
-                }
-                File vidFile = new File(destDir, fileName);
-                try {
-                    Files.copy(f, vidFile);
-                    Findr.logDebug("[ScreenRecordr] " + f.getAbsolutePath() + " => " + vidFile.getAbsolutePath());
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                } finally {
-                    f.delete();
-                }
-            }
-        }
-        return this;
-    }
-
-    public ScreenRecordr removeVideoFiles() {
-        Findr.logDebug("[ScreenRecordr] removing video files");
-        stop();
-        List<File> files = getVideoFiles();
-        for (File f : files) {
-            f.delete();
-        }
-        return this;
-    }
 }
